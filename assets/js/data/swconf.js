61d81240d3c833ce3abe9bfea1243fc390e5df7e
ampls-911 <ampls-911@users.noreply.github.com>
2025-08-19 08:04:19 +0000

    deploy: 5a8a6b511977e3ecc7510cc26ecf4d10564b70dc

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755590651',resources: [
+    cacheName: 'chirpy-1755590658',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
